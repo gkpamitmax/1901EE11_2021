6abddcf07993ddcd1b7c26ddc2de0b05afd292cb
Amit Singh <gkpamitmax@gmail.com>
2021-08-18 22:31:38 +0530

    tut-01

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,1 +1,5 @@
-print("my name is amit singh.my roll no is 1901ee11")
+def meraki_helper(n):
+    """This will detect meraki numner"""
+
+
+input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
